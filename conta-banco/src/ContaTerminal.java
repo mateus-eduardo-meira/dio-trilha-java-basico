ddf08f16cc75ad4ff176cc62b3687fd936354250
mateus-eduardo-meira <mateus.eduardo.meira@gmail.com>
2025-04-25 17:16:27 -0300

    finalização do projeto ContaBanco, que recebe dados via terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        int numero = 0;
+        String agencia = "";
+        String nomeCliente = "";
+        double saldo = 0.0;
+
+        System.out.println("Por favor, digite o número da conta: ");
+        numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        agencia = scanner.next();
+
+        System.out.println("Por favor, digite o seu nome: ");
+        nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite um saldo inicial: ");
+        saldo = scanner.nextDouble();
+
+        System.out.println();
+        
+        System.out.println("\"Olá "+ nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", " +
+        "conta "+ numero +" e seu saldo "+ saldo +" já está disponível para saque\".");
+
     }
 }
